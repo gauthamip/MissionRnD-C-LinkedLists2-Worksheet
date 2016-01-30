bdef4a249cda338a93a3b26a146a18acc0dfa82e
gauthamip <gauthamip@missionrnd.com>
2016-01-30 20:01:53 +0530

    Submit Commit

--- src/merge2LinkedLists.cpp
+++ src/merge2LinkedLists.cpp
@@ -12,12 +12,106 @@ NOTES:
 */
 
 #include <stdio.h>
-
+#include<malloc.h>
 struct node {
 	int num;
 	struct node *next;
 };
 
-struct node * merge2LinkedLists(struct node *head1, struct node *head2) {
-	return NULL;
-}
+struct node * merge2LinkedLists(struct node *head1, struct node *head2){
+		struct node* list, *t, *p;
+		list = NULL;
+		t = NULL;
+		p = NULL;
+		while ((head1 != NULL) || (head2 != NULL))
+		{
+			if (head1 == NULL)
+			{
+				if (list == NULL)
+				{
+					list = (struct node *)malloc(sizeof(struct node*));
+					list->num = head2->num;
+					head2 = head2->next;
+					list->next = NULL;
+				}
+				else
+				{
+					p = list;
+					while (p->next != NULL)
+						p = p->next;
+					t = (struct node *)malloc(sizeof(struct node*));
+					t->num = head2->num;
+					head2 = head2->next;
+					t->next = NULL;
+					p->next = t;
+				}
+
+
+			}
+			else if (head2 == NULL)
+			{
+				if (list == NULL)
+				{
+					list = (struct node *)malloc(sizeof(struct node*));
+					list->num = head1->num;
+					head1 = head1->next;
+					list->next = NULL;
+				}
+				else
+				{
+					p = list;
+					while (p->next != NULL)
+						p = p->next;
+					t = (struct node *)malloc(sizeof(struct node*));
+					t->num = head1->num;
+					head1 = head1->next;
+					t->next = NULL;
+					p->next = t;
+				}
+			}
+			else if ((head1->num) > (head2->num))
+			{
+				if (list == NULL)
+				{
+					list = (struct node *)malloc(sizeof(struct node*));
+					list->num = head2->num;
+					head2 = head2->next;
+					list->next = NULL;
+				}
+				else
+				{
+					p = list;
+					while (p->next != NULL)
+						p = p->next;
+					t = (struct node *)malloc(sizeof(struct node*));
+					t->num = head2->num;
+					head2 = head2->next;
+					t->next = NULL;
+					p->next = t;
+				}
+			}
+			else
+			{
+				if (list == NULL)
+				{
+					list = (struct node *)malloc(sizeof(struct node*));
+					list->num = head1->num;
+					head1 = head1->next;
+					list->next = NULL;
+				}
+				else
+				{
+					p = list;
+					while (p->next != NULL)
+						p = p->next;
+					t = (struct node *)malloc(sizeof(struct node*));
+					t->num = head1->num;
+					head1 = head1->next;
+					t->next = NULL;
+					p->next = t;
+				}
+			}
+		}
+		return list;
+	}
+
